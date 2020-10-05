088bae9f112f6d971a2821acf83f4d05e6adee3c
Xylem <xylem.pl@gmail.com>
2020-10-05 13:20:47 +0200

    Release version 0.15.0

--- dist/main.meta.js
+++ dist/main.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Haxball Headless Server
-// @version 0.14.0
+// @version 0.15.0
 // @author Xylem
 // @match https://*.haxball.com/headless
 // @updateURL https://raw.githubusercontent.com/Xylem/HaxballHeadlessServer/master/dist/main.meta.js
